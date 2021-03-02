90401cb23aa998c3a6f5d75c4c33757f0add8960
AllanCV27 <cahepat27@hotmail.com>
2021-03-02 15:15:32 +0000

    API

--- src/main.py
+++ src/main.py
@@ -8,8 +8,15 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personajes, Planetas, Favoritos
+import datetime
 #from models import Person
+
+## Nos permite hacer las encripciones de contraseñas
+from werkzeug.security import generate_password_hash, check_password_hash
+
+## Nos permite manejar tokens por authentication (usuarios) 
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,6 +26,8 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +39,151 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
+
+    query = Personajes.query.all()
+    results = list(map(lambda personaje: personaje.serialize(), query))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "message": results
     }
-
     return jsonify(response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_personaje(people_id):
+
+    query = Personajes.query.get(people_id)
+    results = query.serialize()
+    response_body = {
+        "message": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    query = Planetas.query.all()
+    results = list(map(lambda planeta: planeta.serialize(), query))
+
+    response_body = {
+        "message": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planeta(planet_id):
+
+    query = Planetas.query.get(planet_id)
+    response_body = {
+        "message": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    query = User.query.all()
+    results = list(map(lambda user: user.serialize(), query))
+
+    response_body = {
+        "message": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+@jwt_required()
+def get_user():
+    user_id = get_jwt_identity()
+    query = Favoritos.query.filter_by(User_id=user_id)
+    results = list(map(lambda favoritos: favoritos.serialize(), query))
+
+    response_body = {
+        "message": results
+    }
+    return jsonify(response_body), 200
+
+@app.route('/users/<int:user_id>/favorites', methods=['POST'])
+def add_favorito(user_id):
+
+    request_favorito = request.get_json()
+    favo = Favoritos(User_id = user_id, planetas_id = request_favorito["planetas_id"], personajes_id = request_favorito["personajes_id"])
+    db.session.add(favo)
+    db.session.commit()
+
+    return jsonify("ok"), 200
+
+@app.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+def delete_favorito(favorite_id):
+
+    del_favorito = Favoritos.query.get(favorite_id)
+    if del_favorito is None:
+        raise APIException('Favorito no encontrado', status_code=404)
+    db.session.delete(del_favorito)
+    db.session.commit()
+
+    return jsonify("ok"), 200
+
+@app.route('/register', methods=["POST"])
+def registro():
+    if request.method == 'POST':
+        username = request.json.get("user", None)
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+        if not username:
+            return jsonify({"message": " El usuario es requerido"}), 400
+        if not email:
+            return jsonify({"message": " El email es requerido"}), 400
+        if not password:
+            return jsonify({"message": "La contraseña es requerida"}), 400
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return jsonify({"message": "Este nombre de usuario ya existe"}), 400
+
+        user = User()
+        user.email = email
+        user.user = username
+        hashed_password = generate_password_hash(password)
+
+        user.password = hashed_password
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"ok": "Gracias. se registro con exito", "status": "true"}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
+
+        if not email:
+            return jsonify({"message": "Nombre de usuario requerido"}), 400
+        if not password:
+            return jsonify({"message": "Contraseña requerida"}), 400
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return jsonify({"message": "Nombre de usuario/contraseña son incorrectas"}), 401
+
+        if not check_password_hash(user.password, password):
+            return jsonify({"message": "Nombre de usuario/contraseña son incorrectas"}), 401
+
+        expiracion = datetime.timedelta(days=1)
+        access_token = create_access_token(identity=user.id, expires_delta=expiracion)
+
+        data = {
+            "user": user.serialize(),
+            "token": access_token,
+            "expires": expiracion.total_seconds()*1000
+        }
+
+        return jsonify(data), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,135 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+#class User(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #email = db.Column(db.String(120), unique=True, nullable=False)
+    #password = db.Column(db.String(80), unique=False, nullable=False)
+    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+
+class Personajes(db.Model):
+    __tablename__ = 'Personajes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    imagen = db.Column(db.String(250))
+    altura = db.Column(db.String(250))
+    masa = db.Column(db.String(250))
+    color_cabello = db.Column(db.String(250))
+    piel = db.Column(db.String(250))
+    ojos = db.Column(db.String(250))
+    fecha_nacimiento = db.Column(db.String(250))
+    genero = db.Column(db.String(250))
+    creacion = db.Column(db.String(250))
+    editado = db.Column(db.String(250))
+    mundo_origen = db.Column(db.String(250))
+    
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+        return{
+            "id":self.id,
+            "imagen":self.imagen,
+            "altura":self.altura,
+            "masa":self.masa,
+            "color_cabello":self.color_cabello,
+            "piel":self.piel,
+            "ojos":self.ojos,
+            "fecha_nacimiento":self.fecha_nacimiento,
+            "genero":self.genero,
+            "creacion":self.creacion,
+            "editado":self.editado,
+            "nombre":self.nombre,
+            "mundo_origen":self.mundo_origen,
+            "personajes_id":self.personajes_id
         }
+
+        def serialize2(self):
+            return{
+                "id":self.id,
+                "name":self.name,
+            }
+
+class Planetas(db.Model):
+    __tablename__ = 'Planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    imagen = db.Column(db.String(250))
+    clima = db.Column(db.String(250))
+    diametro = db.Column(db.String(250))
+    gravedad = db.Column(db.String(250))
+    periodo_orbital = db.Column(db.String(250))
+    poblacion = db.Column(db.String(250))
+    residentes = db.Column(db.String(250))
+    periodo_rotacion = db.Column(db.String(250))
+    superficie_acuatica = db.Column(db.String(250))
+    terreno = db.Column(db.String(250))
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "imagen":self.imagen,
+            "clima":self.clima,
+            "diametro":self.diametro,
+            "gravedad":self.gravedad,
+            "nombre":self.nombre,
+            "periodo_orbital":self.periodo_orbital,
+            "poblacion":self.poblacion,
+            "residentes":self.residentes,
+            "creacion":self.creacion,
+            "periodo_rotacion":self.periodo_rotacion,
+            "superficie_acuatica":self.superficie_acuatica,
+            "terreno":self.terreno,
+            "planetas_id":self.planetas_id
+        }
+    
+    def serialize2(self):
+        return{
+            "id":self.id,
+            "name":self.name,
+        }
+
+class User(db.Model):
+    __tablename__ = 'User'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+   
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "user":self.user,
+            "email":self.email
+        }
+
+class Favoritos(db.Model):
+    __tablename__ = 'Favoritos'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    planetas_id = db.Column(db.Integer, db.ForeignKey('Planetas.id'))
+    personajes_id = db.Column(db.Integer, db.ForeignKey('Personajes.id'))
+    Planetas = db.relationship(Planetas)
+    Personajes = db.relationship(Personajes)
+    User = db.relationship(User)
+
+    
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "User_id":self.User_id,
+            "planetas_id":self.planetas_id,
+            "personajes_id":self.personajes_id
+        }
+ 
